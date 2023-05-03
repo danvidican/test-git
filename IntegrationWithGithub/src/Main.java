61ea411173bed4eaacf880eb403f23a01a41940e
claudiu <claudiu_berchi@yahoo.com>
2023-05-03 12:33:15 -0400

    a new line has been added

--- IntegrationWithGithub/src/Main.java
+++ IntegrationWithGithub/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("This code will be maintained on Github!");
+
+        System.out.println("this line of code was written by clau");
     }
 }
